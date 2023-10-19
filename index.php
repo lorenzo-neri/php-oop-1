0ac8fd692b5fc99557f9fe5fd5b1b125ae80e3e0
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-19 12:45:06 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/* 
+Create un file index.php in cui:
+
+- è definita una classe ‘Movie’ => all’interno della classe sono dichiarate delle variabili d’istanza => all’interno della classe è definito un costruttore => all’interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
+Bonus 1: Modificare la classe Movie in modo che accetti piú di un genere.
+
+Bonus 2: Creare un layout completo per stampare a schermo una lista di movies.
+
+Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
+creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
+mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
+organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
+*/
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
